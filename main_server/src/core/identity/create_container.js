999f899a5d7b8776ce575e405096eb8f4584064d
Ananass <sitthichai@tencent.co.th>
2020-02-19 13:00:59 +0700

    Update eslint tab size

--- main_server/src/core/identity/create_container.js
+++ main_server/src/core/identity/create_container.js
@@ -2,8 +2,14 @@ import errorType from '@error-type';
 
 import * as db from '@db';
 
- async function createContainer(args) {
-    console.log('create_container function execute');
+import uuid from 'uuid/v1';
+
+async function createContainer(args) {
+  const gg = 'wp';
+
+    
+  console.log(uuid());
+  console.log('create_container function execute'+ process.env.AZURE_STORAGE_CONNECTION_STRING);
 }
 
 
--- main_server/src/core/identity/create_identity.js
+++ main_server/src/core/identity/create_identity.js
@@ -5,42 +5,42 @@ import * as db from '@db';
 
 async function createIdentity(args) {
 
-	const transaction = await db.instance().transaction();
+  const transaction = await db.instance().transaction();
 
-	try {
-		const dateNow = new Date();
+  try {
+    const dateNow = new Date();
 
-		const demographic = db.createIdentity({
-			params: {
-				...args,
-				createAt: dateNow
-			},
-			transaction
-		});
+    const demographic = db.createIdentity({
+      params: {
+        ...args,
+        createAt: dateNow
+      },
+      transaction
+    });
 
-		const biometric = db.createIdentityBiometric({
-			params: {
-				...args,
-				livenessImage: base64.base64toBlob(args.livenessImage),
-				branchImage: base64.base64toBlob(args.branchImage)
-			},
-			transaction
-		});
+    const biometric = db.createIdentityBiometric({
+      params: {
+        ...args,
+        livenessImage: base64.base64toBlob(args.livenessImage),
+        branchImage: base64.base64toBlob(args.branchImage)
+      },
+      transaction
+    });
 
-		await Promise.all([demographic, biometric]);
+    await Promise.all([demographic, biometric]);
 
-		await transaction.commit();
+    await transaction.commit();
 
-		return errorType.SUCCESS;
+    return errorType.SUCCESS;
 
-	} catch (error) {
-		if (error) {
-			await transaction.rollback();
+  } catch (error) {
+    if (error) {
+      await transaction.rollback();
 
-			return errorType.FAILED;
-		}
+      return errorType.FAILED;
+    }
 
-	}
+  }
 
 }
 
