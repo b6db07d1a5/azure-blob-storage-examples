4626aaa413ec79e07afe130899b785d6e20429b6
Ananass <sitthichai@tencent.co.th>
2020-02-19 14:17:05 +0700

    Add function blob manage

--- main_server/src/core/identity/create_container.js
+++ main_server/src/core/identity/create_container.js
@@ -8,47 +8,55 @@ import * as db from '@db';
 
 async function createContainer(args) {
 
-  // Create the BlobServiceClient object which will be used to create a container client
-  const blobServiceClient = await BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+// Create the BlobServiceClient object which will be used to create a container client
+	const blobServiceClient = await BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
 
-  // Create a unique name for the container
-  const containerName = 'quickstart' + uuidv1();
+	// Create a unique name for the container
+	const containerName = 'quickstart' + uuidv1();
 
-  console.log('\nCreating container...');
-  console.log('\t', containerName);
+	console.log('\nCreating container...');
+	console.log('\t', containerName);
 
-  //   // Get a reference to a container
-  //   const containerClient = await blobServiceClient.getContainerClient(containerName);
+	//   // Get a reference to a container
+	//   const containerClient = await blobServiceClient.getContainerClient(containerName);
 
-  const containerClient = await blobServiceClient.getContainerClient('quickstart93bbcd30-52dd-11ea-90bf-09c3480f746c');
+	//   const containerClient = await blobServiceClient.getContainerClient('quickstart93bbcd30-52dd-11ea-90bf-09c3480f746c');
 
-  //   // Create the container
-  //   const createContainerResponse = await containerClient.create();
-  //   console.log('Container was created successfully. requestId: ', createContainerResponse.requestId);
+	let i = 1;
+	const iter = blobServiceClient.listContainers();
+	let containerItem = await iter.next();
+	while (!containerItem.done) {
+		console.log(`Container ${i++}: ${containerItem.value.name}`);
+		containerItem = await iter.next();
+	}
 
-  // Create a unique name for the blob
-  const blobName = 'quickstart' + uuidv1() + '.txt';
+	//   // Create the container
+	//   const createContainerResponse = await containerClient.create();
+	//   console.log('Container was created successfully. requestId: ', createContainerResponse.requestId);
 
-  // Get a block blob client
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+	// Create a unique name for the blob
+//   const blobName = 'quickstart' + uuidv1() + '.txt';
 
-  console.log('\nUploading to Azure storage as blob:\n\t', blobName);
+	// Get a block blob client
+//   const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-  // Upload data to the blob
-  const data = 'Hello, World!';
-  const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
-  console.log('Blob was uploaded successfully. requestId: ', uploadBlobResponse.requestId);
+//   console.log('\nUploading to Azure storage as blob:\n\t', blobName);
+
+	// Upload data to the blob
+//   const data = 'Hello, World!';
+//   const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+//   console.log('Blob was uploaded successfully. requestId: ', uploadBlobResponse.requestId);
 
 
-  // List the blob(s) in the container.
+	// List the blob(s) in the container.
 
-  let i = 1;
-  let iter = containerClient.listBlobsFlat();
-  let blobItem = await iter.next();
-  while (!blobItem.done) {
-    console.log(`Blob ${i++}: ${blobItem.value.name}`);
-    blobItem = await iter.next();
-  }
+//   let i = 1;
+//   let iter = containerClient.listBlobsFlat();
+//   let blobItem = await iter.next();
+//   while (!blobItem.done) {
+//     console.log(`Blob ${i++}: ${blobItem.value.name}`);
+//     blobItem = await iter.next();
+//   }
 }
 
 
---
+++ main_server/src/db/azure/blob_storage.js
@@ -0,0 +1,11 @@
+import { BlobServiceClient } from '@azure/storage-blob';
+
+
+export function initialize() {
+	const gg = 'wps';
+	const dddd = 's';
+}
+
+export function instance() {
+
+}
