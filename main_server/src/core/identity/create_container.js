1e20effef4f116cab3d8ba6e31d9ed48247d8a90
Ananass <sitthichai@tencent.co.th>
2020-02-19 13:35:51 +0700

    Add workaroud azure blob

--- main_server/src/core/identity/create_container.js
+++ main_server/src/core/identity/create_container.js
@@ -1,15 +1,54 @@
+import { BlobServiceClient } from '@azure/storage-blob';
+import uuidv1 from 'uuid/v1';
+
 import errorType from '@error-type';
 
 import * as db from '@db';
 
-import uuid from 'uuid/v1';
 
 async function createContainer(args) {
-  const gg = 'wp';
 
-    
-  console.log(uuid());
-  console.log('create_container function execute'+ process.env.AZURE_STORAGE_CONNECTION_STRING);
+  // Create the BlobServiceClient object which will be used to create a container client
+  const blobServiceClient = await BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+
+  // Create a unique name for the container
+  const containerName = 'quickstart' + uuidv1();
+
+  console.log('\nCreating container...');
+  console.log('\t', containerName);
+
+  //   // Get a reference to a container
+  //   const containerClient = await blobServiceClient.getContainerClient(containerName);
+
+  const containerClient = await blobServiceClient.getContainerClient('quickstart93bbcd30-52dd-11ea-90bf-09c3480f746c');
+
+  //   // Create the container
+  //   const createContainerResponse = await containerClient.create();
+  //   console.log('Container was created successfully. requestId: ', createContainerResponse.requestId);
+
+  // Create a unique name for the blob
+  const blobName = 'quickstart' + uuidv1() + '.txt';
+
+  // Get a block blob client
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  console.log('\nUploading to Azure storage as blob:\n\t', blobName);
+
+  // Upload data to the blob
+  const data = 'Hello, World!';
+  const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+  console.log('Blob was uploaded successfully. requestId: ', uploadBlobResponse.requestId);
+
+
+  // List the blob(s) in the container.
+
+  let i = 1;
+  let iter = containerClient.listBlobsFlat();
+  let blobItem = await iter.next();
+  while (!blobItem.done) {
+    console.log(`Blob ${i++}: ${blobItem.value.name}`);
+    blobItem = await iter.next();
+  }
 }
 
 
